c8fcab14a46e1f20b9a83d441909109161fcc789
Esh <esh@any.tv>
2014-07-30 21:13:57 +0800

    update js/Dataservices.js

--- js/DataServices.js
+++ js/DataServices.js
@@ -275,8 +275,7 @@ myExt.factory("dataServices",function($http,$q,$rootScope)
                 var url = liveURL+"templates.php";
 
             $http({method:'GET',
-            // url:url + "?locale=" + chrome.i18n.getUILanguage(),
-            url:url,
+            url:url + "?locale=" + chrome.i18n.getUILanguage(),
             headers:{'Content-Type':'application/data'}
                 }).success(function(data,status,headers,config)
                 {
